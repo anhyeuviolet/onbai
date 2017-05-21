3ea70588f08db1f4dcbdcfe8f742d8fd5418169b
Phan Tan Dung <phantandung92@gmail.com>
2017-05-22 01:26:11 +0700

    Gói cập nhật lên 4.1.02

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hướng dẫn cập nhật module onbai lên phiên bản 4.1.02
+
+Gói cập nhật này hỗ trợ các module onbai sau: 4.1.01
+
+Gói câp nhật này chạy trên NukeViet 4.1.02 nên cần nâng cấp NukeViet lên 4.1.02 trước (Xem hướng dẫn cập nhật tại https://github.com/nukeviet/update/)
+
+## Bước 1: Cập nhật module
+
+### Tùy chọn 1: Cập nhật tự động
+
+Đăng nhập vào quản trị site, vào khu vực Công cụ web => Kiểm tra phiên bản, nếu tại đây có thông báo cần cập nhật module, nhấp vào đó để thực hiện cập nhật tự động. Nếu thất bại hãy thử tùy chọn bên dưới.
+
+### Tùy chọn 2: Cập nhật thủ công
+
+Tải gói cập nhật tại https://github.com/hoaquynhtim99/onbai/releases/download/4.1.02/update_onbai_to4.1.02.zip, sau đó giải nén và upload lên site đúng cấu trúc (thư mục install nhập vào thư mục install trên website).
+Đăng nhập quản trị tối cao, nhận được thông báo cập nhật, nhấp vào link để thực hiện cập nhật.
+
+## Bước 2: Sau cập nhật
+
+Vào phần cấu hình của module, thiết lập lại các giá trị cấu hình và lưu lại.
+Các vấn đề phát sinh thảo luận https://github.com/hoaquynhtim99/onbai/issues
---
+++ install/update_data.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+ * @Project ONBAI 4.X
+ * @Author PHAN TAN DUNG (phantandung92@gmail.com)
+ * @Copyright (C) 2016 PHAN TAN DUNG. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Sat, 11 Jun 2016 23:45:51 GMT
+ */
+
+if (!defined('NV_IS_UPDATE'))
+    die('Stop!!!');
+
+$nv_update_config = array();
+
+// Kieu nang cap 1: Update; 2: Upgrade
+$nv_update_config['type'] = 1;
+
+// ID goi cap nhat
+$nv_update_config['packageID'] = 'NVUDONBAI4102';
+
+// Cap nhat cho module nao, de trong neu la cap nhat NukeViet, ten thu muc module neu la cap nhat module
+$nv_update_config['formodule'] = 'onbai';
+
+// Thong tin phien ban, tac gia, ho tro
+$nv_update_config['release_date'] = 1495390301;
+$nv_update_config['author'] = 'PHAN TAN DUNG (phantandung92@gmail.com)';
+$nv_update_config['support_website'] = 'https://github.com/hoaquynhtim99/onbai/tree/to-4.1.02';
+$nv_update_config['to_version'] = '4.1.02';
+$nv_update_config['allow_old_version'] = array('4.1.01');
+
+// 0:Nang cap bang tay, 1:Nang cap tu dong, 2:Nang cap nua tu dong
+$nv_update_config['update_auto_type'] = 1;
+
+$nv_update_config['lang'] = array();
+$nv_update_config['lang']['vi'] = array();
+
+// Tiếng Việt
+$nv_update_config['lang']['vi']['nv_up_finish'] = 'Đánh dấu phiên bản mới';
+
+$nv_update_config['tasklist'] = array();
+$nv_update_config['tasklist'][] = array(
+    'r' => '4.1.02',
+    'rq' => 1,
+    'l' => 'nv_up_finish',
+    'f' => 'nv_up_finish'
+);
+
+// Danh sach cac function
+/*
+Chuan hoa tra ve:
+array(
+'status' =>
+'complete' => 
+'next' =>
+'link' =>
+'lang' =>
+'message' =>
+);
+status: Trang thai tien trinh dang chay
+- 0: That bai
+- 1: Thanh cong
+complete: Trang thai hoan thanh tat ca tien trinh
+- 0: Chua hoan thanh tien trinh nay
+- 1: Da hoan thanh tien trinh nay
+next:
+- 0: Tiep tuc ham nay voi "link"
+- 1: Chuyen sang ham tiep theo
+link:
+- NO
+- Url to next loading
+lang:
+- ALL: Tat ca ngon ngu
+- NO: Khong co ngon ngu loi
+- LangKey: Ngon ngu bi loi vi,en,fr ...
+message:
+- Any message
+Duoc ho tro boi bien $nv_update_baseurl de load lai nhieu lan mot function
+Kieu cap nhat module duoc ho tro boi bien $old_module_version
+*/
+
+$array_modlang_update = array();
+
+// Lay danh sach ngon ngu
+$result = $db->query("SELECT lang FROM " . $db_config['prefix'] . "_setup_language WHERE setup=1");
+while (list($_tmp) = $result->fetch(PDO::FETCH_NUM)) {
+    $array_modlang_update[$_tmp] = array("lang" => $_tmp, "mod" => array());
+
+    // Get all module
+    $result1 = $db->query("SELECT title, module_data FROM " . $db_config['prefix'] . "_" . $_tmp . "_modules WHERE module_file=" . $db->quote($nv_update_config['formodule']));
+    while (list($_modt, $_modd) = $result1->fetch(PDO::FETCH_NUM)) {
+        $array_modlang_update[$_tmp]['mod'][] = array("module_title" => $_modt, "module_data" => $_modd);
+    }
+}
+
+/**
+ * nv_up_finish()
+ *
+ * @return
+ *
+ */
+function nv_up_finish()
+{
+    global $nv_update_baseurl, $db, $db_config, $nv_Cache, $nv_update_config;
+
+    $return = array(
+        'status' => 1,
+        'complete' => 1,
+        'next' => 1,
+        'link' => 'NO',
+        'lang' => 'NO',
+        'message' => ''
+    );
+
+    try {
+        $num = $db->query("SELECT COUNT(*) FROM " . $db_config['prefix'] . "_setup_extensions WHERE basename='" . $nv_update_config['formodule'] . "' AND type='module'")->fetchColumn();
+        $version = $nv_update_config['to_version'] . " " . $nv_update_config['release_date'];
+        
+        if (!$num) {
+            $db->query("INSERT INTO " . $db_config['prefix'] . "_setup_extensions (
+                id, type, title, is_sys, is_virtual, basename, table_prefix, version, addtime, author, note
+            ) VALUES (
+                5, 'module', 'onbai', 0, 1, 'onbai', 'onbai', '" . $version . "', " . NV_CURRENTTIME . ", 'PHAN TAN DUNG (phantandung92@gmail.com)', 
+                'Module giúp củng cố các kiến thức học tập thông qua 3 cách thức làm kiểm tra: Câu hỏi bắt buộc, câu hỏi thông thường, câu hỏi trắc nghiệm.'
+            )");
+        } else {
+            $db->query("UPDATE " . $db_config['prefix'] . "_setup_extensions SET 
+                id=5, 
+                version='" . $version . "', 
+                author='PHAN TAN DUNG (phantandung92@gmail.com)' 
+            WHERE basename='" . $nv_update_config['formodule'] . "' AND type='module'");
+        }
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+
+    $nv_Cache->delAll();
+    return $return;
+}
